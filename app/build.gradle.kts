cd8a427fb16d9b5190ae687630f169e5891a4840
HalfyDay <gidras13@gmail.com>
2026-07-04 23:01:41 +0800

    Вход в неактивный акк барбера

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 4
-        versionName = "1.0.5"
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
