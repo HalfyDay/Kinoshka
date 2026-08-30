bb637cbba297e31bff661375275f1c6e20ab4cea
HalfyDay <gidras13@gmail.com>
2026-08-30 18:30:03 +0800

    Версия 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 5
-        versionName = "1.0.9"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
